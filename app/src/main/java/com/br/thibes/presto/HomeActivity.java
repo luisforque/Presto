e4ae33063cb3a06d5018726d7ebf7b0d45296e3f
luisforque <luisforque@gmail.com>
2016-09-04 14:53:44 -0300

    starting

---
+++ app/src/main/java/com/br/thibes/presto/HomeActivity.java
@@ -0,0 +1,13 @@
+package com.br.thibes.presto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
