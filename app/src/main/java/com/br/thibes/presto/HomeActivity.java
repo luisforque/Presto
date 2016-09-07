6c0385bc64f12a5d880c9d7121dfca24e49b7c78
luisforque <luisforque@gmail.com>
2016-09-07 10:17:59 -0300

    adicionado libs

--- app/src/main/java/com/br/thibes/presto/HomeActivity.java
+++ app/src/main/java/com/br/thibes/presto/HomeActivity.java
@@ -2,13 +2,29 @@ package com.br.thibes.presto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.botao)
+    Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
+        //botao.setOnClickListener(v -> onClick());
         // teste
+    }
+
+    @OnClick
+    protected void onClick() {
+        Log.d("THIBES", "thibes");
     }
 }
